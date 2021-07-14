7b2891b2a3590c6cc6e7e2685848a3984dd104cd
Jacob Abraham <jacob13400@cet.ac.in>
2021-07-14 13:23:22 +0530

    Cleanup

--- pages/api/incorrect.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  var result = Math.log2((4/3)*(1-req.value))*100;
-  res.statusCode = 200
-  res.json({ message: 'Hey!\nThis is the co-author of this site. Nice to meet you\nBye!',
-             value: result
-        })
-}
