2beb012ad750ae512d4c9c52406b34329994af08
Jacob Abraham <jacob13400@cet.ac.in>
2021-07-16 20:22:50 +0530

    get Questions Sync

---
+++ pages/api/questions.js
@@ -0,0 +1,19 @@
+import admin from '../../lib/clientApp'
+
+export const getProfileData = async () => {
+  const db = admin.database()
+  const questionsDB = db.ref('questions')
+  
+  questionsDB.get().then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+      return snapshot.val();
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+
+  return null;
+}
