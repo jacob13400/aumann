28374ffcd730178e2dab4b8697e1c790178053db
Jozdien <jozdien@gmail.com>
2021-07-20 23:17:08 +0530

    Made components to make the large Home and Room pages more readable.

---
+++ component/Modals/PlayModal/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import styles from './styles.module.css';
+
+import Button from '../../Button';
+
+export default function PlayModal(props) { 
+  const [formState, setFormState] = React.useState({
+    roomID: "",
+    username: "",
+  });
+
+
+  const onEnter = () => {
+    localStorage.setItem("username", formState.username);
+    localStorage.setItem("roomID", formState.roomID);
+
+    fetch("http://localhost:3000/api/room", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formState),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        Router.push("/room");
+      })
+      .catch(console.log);
+
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Enter Room ID
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="roomID">
+            <Form.Label>Room ID</Form.Label>
+            <Form.Control 
+              type="text" 
+              placeholder="eg: AEF123" 
+              onChange={(e) => 
+                setFormState({ ...formState, roomID: e.target.value})
+              }
+              value={formState.roomID}
+            />
+          </Form.Group>
+          <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control 
+              type="text" 
+              placeholder="Enter your name" 
+              onChange={(e) => 
+                setFormState({ ...formState, username: e.target.value})
+              }
+              value={formState.username}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button type={"modal"} text={"Enter"} action={onEnter}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
---
+++ component/RoomFooter/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export default function RoomFooter(props) {
+  return (
+    <div className={styles.bottom}>
+      <div className={styles.timer}>
+        05:00
+      </div>
+    </div>
+  )
+}
