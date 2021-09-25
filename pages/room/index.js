2e9c42c892b7a79f92c8bf79139d0c3383b5f284
Jozdien <jozdien@gmail.com>
2021-09-25 16:28:04 +0530

    Changes to room

--- pages/room/index.js
+++ pages/room/index.js
@@ -29,9 +29,7 @@ export default function Room(props) {
   const getQuestions = async () => {
     var questions = await getQuestionsSync();
 
-    console.log(questions);
-
-    setTimeout(function(){console.log("Questions: ", questions);}, 3000);
+    setTimeout(function(){console.log("Questions: ", questions)}, 3000);
   };
 
   const getUsersList = async () => {
