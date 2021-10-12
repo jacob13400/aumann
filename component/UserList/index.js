15fd311aa86d429557f494f3063754c58ddc8028
Jozdien <jozdien@gmail.com>
2021-10-12 20:48:39 +0530

    Random colours for users in room.

--- component/UserList/index.js
+++ component/UserList/index.js
@@ -1,6 +1,8 @@
 import styles from './styles.module.css';
 
 export default function UserList(props) {
+  const colours = ["#EF476F", "#06D6A0", "#FB9F89", "#8EEDF7"];
+
   return (
     <div className={styles.listContainer}>
       <div className={styles.list}>
@@ -15,7 +17,7 @@ export default function UserList(props) {
                   #{index + 1}
                 </div>
                 <div className={styles.playerDetails}>
-                  <div style={{color: player.color}}>
+                  <div style={{color: colours[index]}}>
                     {player.username}
                     {player.username == props.username ? " (You)" : null}
                   </div>
